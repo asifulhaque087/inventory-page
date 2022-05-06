3b7bb1c2f533a7ec950e97b95b020847307b588e
asiful087 <asifulhaque087@gmail.com>
2022-05-07 04:20:12 +0600

    add theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#00C614",
+        "neutral-9": "#3E3F48",
+        "neutral-7": "#6B6C72",
+        "neutral-8": "#88898E",
+      },
+    },
   },
   plugins: [],
-}
+};
