2106c8f8b5e5fc87b3d16feffe9886dd96163602
asiful087 <asifulhaque087@gmail.com>
2022-05-07 02:45:08 +0600

    add iconscout

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import UilGithub from "@iconscout/react-unicons/icons/uil-github";
 
 const Inventory = () => {
   return (
-    <div>Inventory page</div>
-  )
-}
+    <>
+      <div>Inventory page</div>
+      <div>
+        <UilGithub size="140" color="#61DAFB" />
+      </div>
+    </>
+  );
+};
 
-export default Inventory
+export default Inventory;
